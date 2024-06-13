526092214c2dd34fc2c31b00c491305883202cc1
Jordan <jordan@smartdust.me>
2024-06-13 09:36:23 +0200

    Fix camera url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header>
         <h1>Jordan's robot remote control</h1>
-        <img src="http://192.168.40.244:9000/stream.mjpg" width="640" height="480"/>
+        <img src="http://raspberrypi.local:9000/stream.mjpg" width="640" height="480"/>
         <JoystickControl></JoystickControl>
       </header>
     </div>
