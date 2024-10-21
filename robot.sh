e883148fab7ad6e36bea1f3d2f2bb4cf2943db87
Jordan Parviainen <jordan.parviainen.stud@pw.edu.pl>
2024-10-21 12:59:08 +0100

    Added camera stream server

--- robot.sh
+++ robot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 fuser -k 8000/tcp
 fuser -k 3000/tcp
-sudo nmcli con up Hotspot
+#sudo nmcli con up Hotspot
 cd /home/pi/dyplom/build
 (trap 'kill 0' SIGINT; python -m http.server 3000 & cd ..; ./motor.py)
