3123106c70034d553d10ba784fc9e1da43c28ca9
Jordan <jordan@smartdust.me>
2025-01-05 22:23:08 +0100

    New frontend build and updated startup script

--- camera-service/main.py
+++ camera-service/main.py
@@ -8,6 +8,7 @@ from camera_app import camera_streaming_app
 if __name__ == "__main__":
     camera = Picamera2()
     camera_manager = CameraManager(camera)
-    camera_app = camera_streaming_app(camera_manager)
+    auth_url = os.getenv("AUTHENTICATION_URL", "http://localhost:5000/api/auth")
+    camera_app = camera_streaming_app(camera_manager, auth_url)
     camera_port = int(os.getenv("CAMERA_SERVICE_EXT_PORT", 8000))
     uvicorn.run(camera_app, host="0.0.0.0", port=camera_port)
--- robot.sh
+++ robot.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
+PROJ_PATH=/home/jordan/studia/dyplom/robot-control
+cd $PROJ_PATH/frontend/build
 fuser -k 8080/tcp
 fuser -k 8081/tcp
+fuser -k 8000/tcp
 fuser -k 3000/tcp
-#sudo nmcli con up Hotspot
-cd /home/pi/dyplom/build
-(trap 'kill 0' SIGINT;
-python -m http.server 3000 & cd ..; ./motor.py)
+PYTHON=/home/jordan/studia/dyplom/robot-control/venv/bin/python
+trap 'kill 0' SIGINT
+$PYTHON -m http.server 3000 &
+cd $PROJ_PATH/motor-service && $PYTHON main.py &
+cd $PROJ_PATH/camera-service && $PYTHON test.py &
+cd $PROJ_PATH && $PYTHON app.py
+wait
